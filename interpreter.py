615f332a4c257348794916be789271987b0f1c8c
lioraR <liora.rosenberg@planet.nl>
2018-11-08 16:37:09 +0100

    verbeterde interpreter

--- interpreter.py
+++ interpreter.py
@@ -2,31 +2,29 @@ import csv
 from bs4 import BeautifulSoup
 
 # importeer usabuurlanden
-INPUT_CSV = 'usabuurlanden.csv'
-
-
-# pak het eerste item van de csv
-states_list = []
-
-#state_list.append(usabuurlanden[0])
-
+INPUT_CSV = "buurlanden_NL.csv"
 
 
 if __name__ == "__main__":
     # open usabuurlanden
     with open(INPUT_CSV, newline='') as csvfile:
-        reader = csv.DictReader(csvfile)
+        #reader = csv.DictReader(csvfile)
+        lines = csvfile.readlines()
+        print(lines[0])
 
-        []
+        for i in lines:
+            provinces = i.split(';')
+            province_neigbours = i.split(',')
+            print(provinces)
+            print(province_neigbours)
+
+        #split
+        #state_list = []
+
+        #
+        #    state_list.append()
 
 
-def save_csv(outfile, movies):
-    """
-    Output a CSV file containing highest rated movies.
-    """
-    writer = csv.writer(outfile)
-    writer.writerow(['Title', 'Rating', 'Year', 'Actors', 'Runtime'])
-
-    # movies are assigned to disk
-    for movie in range(len(movies[0])):
-        writer.writerow([movies[0][movie], movies[1][movie], movies[2][movie], movies[3][movie], movies[4][movie]])
+# pak het eerste item van de csv
+#states_list = []
+#state_list.append(buurlanden_NL[0])

5bbc14b3e3d9e3fae217ddb8fdddac7f97ccc709
lioraR <liora.rosenberg@planet.nl>
2018-11-08 14:42:11 +0100

    begin interpreter

--- interpreter.py
+++ interpreter.py
@@ -1,1 +1,18 @@
-kdk
+import csv
+from bs4 import BeautifulSoup
+
+# importeer usabuurlanden
+INPUT_CSV = 'usabuurlanden.csv'
+
+# open usabuurlanden
+with open('usabuurlanden.csv', newline='') as csvfile:
+    read = csv.DictReader(csvfile)
+
+list =[]
+
+for row in read:
+    list.append(row)
+
+
+if __name__ == "__main__":
+    print(list)

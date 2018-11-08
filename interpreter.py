cc6e34f4084040fe5d864929b04de5df4ef4299a
lioraR <liora.rosenberg@planet.nl>
2018-11-08 15:50:34 +0100

    mapjes

--- interpreter.py
+++ interpreter.py
@@ -4,15 +4,29 @@ from bs4 import BeautifulSoup
 # importeer usabuurlanden
 INPUT_CSV = 'usabuurlanden.csv'
 
-# open usabuurlanden
-with open('usabuurlanden.csv', newline='') as csvfile:
-    read = csv.DictReader(csvfile)
 
-list =[]
+# pak het eerste item van de csv
+states_list = []
 
-for row in read:
-    list.append(row)
+#state_list.append(usabuurlanden[0])
+
 
 
 if __name__ == "__main__":
-    print(list)
+    # open usabuurlanden
+    with open(INPUT_CSV, newline='') as csvfile:
+        reader = csv.DictReader(csvfile)
+
+        []
+
+
+def save_csv(outfile, movies):
+    """
+    Output a CSV file containing highest rated movies.
+    """
+    writer = csv.writer(outfile)
+    writer.writerow(['Title', 'Rating', 'Year', 'Actors', 'Runtime'])
+
+    # movies are assigned to disk
+    for movie in range(len(movies[0])):
+        writer.writerow([movies[0][movie], movies[1][movie], movies[2][movie], movies[3][movie], movies[4][movie]])

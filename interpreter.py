5009471782cb384e9ad8fce57b4b6ff1a68a4172
lioraR <liora.rosenberg@planet.nl>
2018-11-09 14:24:08 +0100

    bijna klaar

--- interpreter.py
+++ interpreter.py
@@ -1,50 +1,43 @@
+# Name: Liora Rosenberg
+# Student number: 11036435
+"""
+This script interprets an excel file and then outputs a list of provinces (list_provinces) and a list of neighbouring provinces (province_neigbour). 
+"""
+
 import csv
 from bs4 import BeautifulSoup
 
-# importeer usabuurlanden
+# import csv file
 INPUT_CSV = "buurlanden_NL.csv"
 
-
 if __name__ == "__main__":
-    # open usabuurlanden
+
+    # open excel file
     with open(INPUT_CSV, newline='') as csvfile:
-        #reader = csv.DictReader(csvfile)
         lines = csvfile.readlines()
         #print(lines[0])
 
-        #provinces = lines.split(';')
-        #province = provinces[-1].strip('/r/n')
-        #province_neigbours = lines.split(',')
-        #province_neigbour = province_neigbours[-1].strip('/r/n')
+        list_provinces = []
 
+        # loop over lines to get (neighboring) provinces
         for line in lines:
             split_list = line.split(';')
             provinces = split_list[0].strip('/r/n')
-            print(provinces)
+            #print(type(provinces))
 
-            #province = provinces[line]
-            #print(province)
-            #print(province, end=(": "))
-            #print( )
+            # The provinces were strings and I put it in a list
+            list_provinces.append(provinces)
 
             province_neigbours = split_list[1]
-            #print(province_neigbours)
-            #province_neigbours_without_enters = province_neigbours[-1].strip('/r/n')
             province_neigbour = province_neigbours.split(',')
 
-            province_neigbour[-1] = province_neigbour[-1].strip('/r/n')
+            province_neigbour[-1] = province_neigbour[-1].rstrip()
             print(province_neigbour)
-            #print(type(province_neigbour))
 
-        #for j in range(len(lines)):
-        #    print(provinces[j])
-        #    print(province_neigbour[j])
+        print(list_provinces)
+
+
+
 
 # lijst maken van de provincies
 # lijst maken van province_neigbour
-
-
-
-# pak het eerste item van de csv
-#states_list = []
-#state_list.append(buurlanden_NL[0])

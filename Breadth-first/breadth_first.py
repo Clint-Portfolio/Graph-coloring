4fe968fec14a9348f71a3d5aaeba4d22c78fb5f9
lioraR <liora.rosenberg@planet.nl>
2018-11-13 11:40:49 +0100

    work in progress

---
+++ Breadth-first/breadth_first.py
@@ -0,0 +1,22 @@
+# Name: Liora Rosenberg
+# Student number: 11036435
+"""
+This script interprets an excel file and then outputs a list of provinces (list_provinces) and a list of neighbouring provinces (province_neigbour).
+"""
+
+import csv
+from bs4 import BeautifulSoup
+#import functie from documentnaam
+
+from helpers import provinces
+from helpers import land_naar_nummer
+#import provinces from helpers
+#import land_naar_nummer from helpers
+
+print(buurland_provincie)
+#search root of the graph and add to queue + # begin bij de meest linker van het rijtje
+
+#if there is a node:
+	#if there is a solution: stop searching and give a solution
+	#if there is not a solution: add all children
+# if queue is empty, stop searching

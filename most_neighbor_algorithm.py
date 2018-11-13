9d804ec67fdd634e5e60bcacd2074273e34e2845
Manhut69 <44084866+Manhut69@users.noreply.github.com>
2018-11-13 10:45:24 +0100

    update for super deluxe graph

--- most_neighbor_algorithm.py
+++ most_neighbor_algorithm.py
@@ -11,13 +11,13 @@ countrylist = []
 transmitter_list = ["A", "B", "C", "D", "E"]
 
 graphtype = ""
-while graphtype not in ["simple", "single", "double"]:
-    print("Graphtype (simple, single, double)")
-    graphtype = input(" >").lower()
+while graphtype not in ["simple", "single", "double", "triple"]:
+    print("Graphtype (simple, single, double, triple)")
+    graphtype = input("> ").lower()
 
 nr_of_nodes = 0
-while nr_of_nodes < 4:
-    nr_of_nodes = int(input("number of nodes (at least 4): "))
+while nr_of_nodes < 5:
+    nr_of_nodes = int(input("number of nodes (at least 5: "))
 
 if graphtype == "simple":
     # generate a simple graph of type:
@@ -47,7 +47,7 @@ elif graphtype == "single":
 
     countrylist.append(Node(len(countrylist), None, [0, 1, 2]))
 
-else:
+elif graphtype == "triple":
     # generate a double triple edge graph of type:
     #   0
     # / | \
@@ -69,12 +69,41 @@ else:
     countrylist.append(Node(len(countrylist), None, [0, 1, 2]))
     countrylist.append(Node(len(countrylist), None, [1, 2, 3]))
 
+else:
+    # generate a triple node graph of type:
+    #   0----0
+    # / | \/ |\
+    # 0-0-0-0-0-0-... etc.
+    #   \ | /
+    #     0
+    nodetop1 = Node(nr_of_nodes, None, [0, 1, 2, nr_of_nodes + 1])
+    nodetop2 = Node(nr_of_nodes + 1, None, [2, 3, 4, nr_of_nodes])
+    nodebottom = Node(nr_of_nodes + 2, None, [1, 2, 3])
+
+    for i in range(0, nr_of_nodes):
+        if i == 0:
+            countrylist.append(Node(i, None, [1, nodetop1.name]))
+        elif i == 1:
+            countrylist.append(Node(i, None, [i - 1, i + 1, nodetop1.name, nodebottom.name]))
+        elif i == 2:
+            countrylist.append(Node(i, None, [i - 1, i + 1, nodetop1.name, nodebottom.name, nodetop2.name]))
+        elif i == 3:
+            countrylist.append(Node(i, None, [i - 1, i + 1, nodebottom.name, nodetop2.name]))
+        elif i == 4:
+            countrylist.append(Node(i, None, [i - 1, i + 1, nodetop2.name]))
+        elif i == nr_of_nodes - 1:
+            countrylist.append(Node(i, None, [i - 1]))
+        else:
+            countrylist.append(Node(i, None, [i - 1, i + 1]))
+
+        countrylist.append(nodetop1)
+        countrylist.append(nodetop2)
+        countrylist.append(nodebottom)
 
 # find node with most neighbors
 most_neighbored_countries = []
 for node in countrylist:
     if len(node.neighbors) > neighborcount:
-        # most_neighbored_countries.append(countrylist.index(node))
         neighborcount = len(node.neighbors)
 
 for node in countrylist:
@@ -90,12 +119,16 @@ for node in countrylist:
     if node.trans_type == None:
         node.changetype(transmitter_list, countrylist)
 
-# for node in most_neighbored_countries:
-#     countrylist[node].changetype(transmitter_list, countrylist)
-if graphtype in ["single", "double"]:
-    print(" " + countrylist[-2].trans_type)
-for node in countrylist[:-2]:
+for node in most_neighbored_countries:
+    countrylist[node].changetype(transmitter_list, countrylist)
+
+if graphtype in ["single", "double", "triple"]:
+    print(" " + countrylist[-3].trans_type, end = "")
+    if graphtype == "triple":
+        print(" " + countrylist[-1].trans_type, end = "")
+print()
+for node in countrylist[:-3]:
     print(node.trans_type, end = "")
-if graphtype == "double":
+if graphtype in ["double", "triple"]:
     print()
-    print("  " + countrylist[-1].trans_type)
+    print("  " + countrylist[-2].trans_type)

b322ecc217bc1f5e8dea797b7058a9a9ad706310
lioraR <liora.rosenberg@planet.nl>
2018-11-20 17:23:22 +0100

    update depth first

--- Code/Algoritmes/Breadth-first/depth-first.py
+++ Code/Algoritmes/Breadth-first/depth-first.py
@@ -10,27 +10,24 @@ from bs4 import BeautifulSoup
 #import functie from documentnaam
 from helpers import *
 
-#province_list = (provinces("buurlanden_NL.csv"))[0]
-#print(province_list)
-
-#list_neighbors = (provinces("buurlanden_NL.csv"))[1]
-#print(list_neighbors)
-
 list_color = ['A','B','C','D','E']
 list_color_node = []
-
-# pak node 0: meest linker provincie
-#left_provinces = province_list[0]
-#print(province_list[0])
-
+grand_list = []
 
 # buren en daar stop je 0 in en je lijst van buren. je kijkt in de lijst wat de buren van. recursief
 def depth_first(list_neigbors, current_node, list_color_node):
     list_color_node[current_node] = True
     for buurland in list_neigbors[current_node]:
         print(buurland)
+
         if list_color_node[buurland] == None:
             return(depth_first(list_neigbors, buurland, list_color_node))
+
+#def color():
+    list_color_node[current_node] = True
+    #check hoe lang de lijst is
+    #for i in lijst verwijder eerste lijst
+    #ga naar volgende buur
 
 if __name__ == '__main__':
     list_neigbors = generate_triple(True)
@@ -41,24 +38,3 @@ if __name__ == '__main__':
         print(list_color_node)
 
     depth_first(list_neigbors, 0, list_color_node)
-
-
-
-
-
-
-        # if province has no neigbours stop
-        #if list_neigbors = []:
-        #    return
-        # if province has neigbours find them
-        #else:
-        #    return(provinces(list_neigbors[buurland-1]))
-        # if neigbouringprovince for original province has no neigbourprovinces go back to original province and find neigbouringprovince
-
-
-#def provincies()
-#a = provinces("buurlanden_NL.csv")
-#nummers = land_naar_nummer(a[0], a[1])
-#print(nummers)
-
-# def check_color(list_color)

4b57f895d403975ee1b0f4020d083e5fbe331453
slagtkracht <r.slagt@outlook.com>
2018-11-21 10:13:05 +0100

    main function

---
+++ Code/main.py
@@ -0,0 +1,15 @@
+# Add the file-structure to paths
+import os, sys
+directory = os.path.dirname(os.path.realpath(__file__))
+sys.path.append(os.path.join(directory, "Algoritmes"))
+sys.path.append(os.path.join(directory, "Algoritmes", ""))
+
+#imports
+import helpers
+
+#code
+def main():
+    if x:
+        run random
+    else:
+        run greedy

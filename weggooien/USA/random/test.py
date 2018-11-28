04a189aed2c0e9ea26713e9728364c57bfe0cbd3
Manhut69 <kenpokaratefan@gmail.com>
2018-11-28 12:14:12 +0100

    rm'd all redundant helper scripts and removed 'weggooien'

--- weggooien/USA/random/test.py
+++
@@ -1,7 +0,0 @@
-zendmast_met_land = {}
-    
-for ipoging in range(10):
-        
-        for provincie in provincies: 
-            zendmast = random.randrange(7)
-            zendmast_met_land [provincie] = zendmast
